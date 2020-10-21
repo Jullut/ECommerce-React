7166e60df227f6d5be2cc0246f636e645bce70a3
julia <jullut.jullut@gmail.com>
2020-10-21 18:30:41 +0300

    finished working on category landing services

--- src/components/LandingServices/index.js
+++ src/components/LandingServices/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import styles from './LandingServices.module.scss';
+import Filter from '../Filter';
+import CustomCard from '../CustomCard';
 import { Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Breadcrumb from 'react-bootstrap/Breadcrumb';
-import Dropdown from 'react-bootstrap/Dropdown';
 import CardColumns from 'react-bootstrap/CardColumns';
-import CustomCard from '../CustomCard';
+import Accordion from 'react-bootstrap/Accordion';
+import Card from 'react-bootstrap/Card';
 
 const cards = [
 	{
@@ -92,6 +94,29 @@ const cards = [
 	}
 ];
 
+const cardsSm = [
+	{
+		title: 'Kristina Dam Oak Table With White Marble Top',
+		price: '$ 799.55',
+		cardImg: '/img/popularItems/item3.png'
+	},
+	{
+		title: 'EAMES x Cast + Crew - Custom Powder Coated ',
+		price: '$ 799.55',
+		cardImg: '/img/popularItems/item1.png'
+	},
+	{
+		title: 'Activate Facial Mask and Charcoal Soap ',
+		price: '$ 129.55',
+		cardImg: '/img/popularItems/item10.png'
+	},
+	{
+		title: 'Vitra Cork Stool B, Cork - Design Within Reach',
+		price: '$ 299.99',
+		cardImg: '/img/popularItems/item10.png'
+	}
+];
+
 const LandingServices = () => {
 	return (
 		<div>
@@ -113,88 +138,60 @@ const LandingServices = () => {
 					</Col>
 				</Row>
 
-				<div className={styles.bg}>
-					<Row className={styles.row}>
-						<Col>
-							<Row>
-								<Col>
-									<span className={styles.dropTitle}>Price</span>
-								</Col>
-								<Col className={styles.col}>
-									<Dropdown className={styles.dropdown}>
-										<Dropdown.Toggle id="dropdown-basic">$0.00 — $100.00</Dropdown.Toggle>
-									</Dropdown>
-								</Col>
-							</Row>
-							<Row className={styles.smRow}>
-								<Col>
-									<span className={styles.dropTitle}>Color</span>
-								</Col>
-								<Col className={styles.col}>
-									<Dropdown className={styles.dropdown}>
-										<Dropdown.Toggle id="dropdown-basic">Dropdown Button</Dropdown.Toggle>
-									</Dropdown>
-								</Col>
-							</Row>
-						</Col>
-						<Col>
-							<Row>
-								<Col>
-									<span className={styles.dropTitle}>Brands</span>
-								</Col>
-								<Col className={styles.col}>
-									<Dropdown className={styles.dropdown}>
-										<Dropdown.Toggle id="dropdown-basic">Ukraine sport</Dropdown.Toggle>
-									</Dropdown>
-								</Col>
-							</Row>
-							<Row className={styles.smRow}>
-								<Col>
-									<span className={styles.dropTitle}>Sort by</span>
-								</Col>
-								<Col className={styles.col}>
-									<Dropdown className={styles.dropdown}>
-										<Dropdown.Toggle id="dropdown-basic">Price</Dropdown.Toggle>
-									</Dropdown>
-								</Col>
-							</Row>
-						</Col>
-						<Col>
-							<Row>
-								<Col>
-									<span className={styles.dropTitle}>Size</span>
-								</Col>
-								<Col className={styles.col}>
-									<Dropdown className={styles.dropdown}>
-										<Dropdown.Toggle id="dropdown-basic">Small</Dropdown.Toggle>
-									</Dropdown>
-								</Col>
-							</Row>
-							<Row className={styles.smRow}>
-								<Col>
-									<span className={styles.dropTitle}>Show</span>
-								</Col>
-								<Col className={styles.col}>
-									<Dropdown className={styles.dropdown}>
-										<Dropdown.Toggle id="dropdown-basic">16 per page</Dropdown.Toggle>
-									</Dropdown>
-								</Col>
-							</Row>
-						</Col>
-					</Row>
+				<Row className="d-xs-flex d-sm-flex d-md-none">
+					<Col>
+						<Accordion defaultActiveKey="0">
+							<Card className={styles.card}>
+								<Card.Header className={styles.cardHeader}>
+									<Accordion.Toggle variant="link" eventKey="0">
+										Hide Filter
+									</Accordion.Toggle>
+								</Card.Header>
+								<Accordion.Collapse eventKey="0">
+									<Card.Body className={styles.cardBody}>
+										<Filter />
+									</Card.Body>
+								</Accordion.Collapse>
+							</Card>
+						</Accordion>
+					</Col>
+				</Row>
+
+				<div className="d-none d-md-flex">
+					<Filter />
 				</div>
 
 				<Row>
 					<Col>
+						<p className={styles.results}>
+							<span className={styles.resultNum}>12,931</span> results in apparel
+						</p>
+					</Col>
+				</Row>
+
+				<Row>
+					<Col className="d-none d-md-flex">
 						<CardColumns className={styles.cardColumns}>
 							{cards.map(({ title, price, cardImg }) => (
 								<CustomCard cardImg={cardImg} title={title} price={price} />
 							))}
 						</CardColumns>
 					</Col>
+
+					<Col className="d-xs-flex d-sm-flex d-md-none">
+						<CardColumns className={styles.cardColumns}>
+							{cardsSm.map(({ title, price, cardImg }) => (
+								<CustomCard cardImg={cardImg} title={title} price={price} />
+							))}
+						</CardColumns>
+					</Col>
 				</Row>
 
-				<Row>a</Row>
+				<Row>
+					<Col>
+						<button className={styles.btn}>load more</button>
+					</Col>
+				</Row>
 			</Container>
 		</div>
 	);
